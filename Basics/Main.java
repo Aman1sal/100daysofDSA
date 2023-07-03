b9fd7f1fa08fc4f3849c7eee670bcebacb40a8d3
Aman <aman8958bansal@gmail.com>
2023-07-03 18:34:17 +0530

    added basics of java

---
+++ Basics/Main.java
@@ -0,0 +1,160 @@
+// import java.util.Arrays;
+// import java.util.Scanner;
+public class Main {
+    public static void printJava(){
+        System.out.println("Hello java");
+    }
+    public static void printName(String name){
+        System.out.println(name);
+    }
+    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+//        System.out.println("Hello Aman");
+//            String name = "Aman";
+//            int age  = 22;
+//            String friend = name;
+//        System.out.println(name);
+        // // Primitive data types
+//        byte age  = 30;
+//        int phone = 9293948;
+//        long phone2 = 999999999999L;
+//        float pi = 3.14F;
+//        char letter = 'c';
+//        boolean isAdult = true;
+
+        // // // NonPrimitive Data Types
+//        String name = "Aman";
+//        System.out.println(name.length());
+
+//        // // // String methods
+//        String name1 = "Aman";
+//        String name2 = "Akku";
+//        String name3 = name1 +"and"+ name2;
+//        System.out.println(name3);
+//        System.out.println(name1.charAt(0));
+//        System.out.println(name1.length());
+//        String name4 = name2.replace('k','b');
+//        System.out.println(name4);
+//        System.out.println(name3.substring(0,4));
+
+//        // // // Arrays
+
+//        int[] marks = new int[3];
+//        marks[0] = 97;
+//        marks[1] = 84;
+//        marks[2] = 88;
+//        System.out.println(marks[1]);
+//        System.out.println(marks.length);
+//        Arrays.sort(marks);
+//        System.out.println(marks[1]);
+
+//        int[] marks = {98, 84, 88};
+//        System.out.println(marks[1]);
+//
+//        int[][] finalMarks = {{97, 87, 83},{88, 90, 94}};
+//        System.out.println(finalMarks[0][0]);
+
+//        // // // Casting
+//        double price = 100.00;
+//        double finalPrice = price + 18;
+//        System.out.println(finalPrice);
+//
+//        int p = 100;
+//        int fp = p + (int)18.00;
+//        System.out.println(fp);
+
+
+        //// // Constants
+//        final float PI = 3.14F;
+
+//        // // opeartors
+//        int num = 1;
+//        num++;
+//        System.out.println(num);
+
+        // // //Maths
+//        System.out.println(Math.max(5,6));
+//        System.out.println(Math.min(5,6));
+//        System.out.println((int)(Math.random()*100));
+
+        // // Taking Input
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Input your age");
+////        int age = sc.nextInt();
+////        System.out.println(age);
+//        // // String ko input krana ho to: -> keval pehla word of sentence print krega
+////        String name = sc.next();
+////        System.out.println(name);
+//        // // // poori line k liye;
+//        String name = sc.nextLine();
+//        System.out.println(name);
+
+//        // // Conditionals
+//        boolean isSunUp = true;
+//        if(isSunUp==true)
+//            System.out.println("day");
+//        else
+//            System.out.println("night");
+
+//        int age = 18;
+//        if(age>=18)
+//            System.out.println("Can Vote");
+//        else
+//            System.out.println("baccha h tu");
+
+//            Scanner sc = new Scanner(System.in);
+//            int cash = sc.nextInt();
+//            if(cash<10){
+//                System.out.println("Cant buy anything");
+//                System.out.println("get more cash");
+//            }
+//            else if(cash>10 && cash<50){
+//                System.out.println("can get only pen");
+//            }
+//            else {
+//                System.out.println("can get anyhting");
+//            }
+
+//        // // // loops
+//        for(int i=100; i>=1; i--){
+//            System.out.println(i);
+//        }
+//        int i = 100;
+//        while (i>=1){
+//            System.out.println(i);
+//            i--;
+//        }
+//        int k = 100;
+//        do {
+//            System.out.println(k);
+//            k = k-1;
+//        } while (k>=1);
+
+//        int i = 0;
+//        while(true){
+//            if(i==3){
+//                i = i+1;
+//                continue;
+//            }
+//            System.out.println(i);
+//            i = i+1;
+//            if(i>5){
+//                break;
+//            }
+//        }
+
+        // // // Exception handling
+//        int[] marks = {97, 96, 92};
+//        try {
+//            System.out.println(marks[5]);
+//        } catch (Exception exception) {
+//            System.out.println("The student name is Aman");
+//        }
+
+//        // // // Methods and functions
+//        printJava();
+       printJava();
+       printJava();
+       printName("Aman");
+    }
+}
