1d559a04d385d702b239e22c6fa9a2c9b940a642
Aman <aman8958bansal@gmail.com>
2023-08-28 22:50:33 +0530

    recursion

---
+++ Recursion L1/src/SumOfDigits.java
@@ -0,0 +1,11 @@
+public class SumOfDigits {
+    public static void main(String[] args) {
+        System.out.println(SOD(1342));
+    }
+    static int SOD(int n){
+        if(n==0){
+            return 0;
+        }
+        return (n%10) + SOD(n/10);
+    }
+}
