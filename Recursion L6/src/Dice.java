2a844139a676876567782af6bcfc2c9acaf30867
Aman <aman8958bansal@gmail.com>
2023-09-03 12:55:30 +0530

    Recursion

---
+++ Recursion L6/src/Dice.java
@@ -0,0 +1,15 @@
+public class Dice {
+    public static void main(String[] args) {
+        dicecomb("", 4);
+    }
+    static void dicecomb(String p, int target){
+        if(target ==0){
+            System.out.println(p);
+            return;
+        }
+
+        for(int i=1; i<=6 && i<=target; i++){
+            dicecomb(p+i, target-i);
+        }
+    }
+}
