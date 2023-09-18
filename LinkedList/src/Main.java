57f5e95947615340ef499037ffbf0e1555c957ed
Aman <aman8958bansal@gmail.com>
2023-09-18 23:33:29 +0530

    Linked List

---
+++ LinkedList/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+//        LL list = new LL();
+//        list.insertFirst(3);
+//        list.insertFirst(8);
+//        list.insertFirst(7);
+//        list.insertFirst(17);
+//        list.insertlast(99);
+//        list.insert(100,3);
+//        System.out.println(list.deleteFirst());
+//        list.display();
+//
+//        list.deleteLast();
+//        list.display();
+//
+//        System.out.println(list.delete(2));
+//        list.display();
+
+        DoubllyLL list = new DoubllyLL();
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(8);
+        list.insertFirst(19);
+        list.insertLast(99);
+        list.insert(8, 73);
+
+        list.display();
+    }
+}
